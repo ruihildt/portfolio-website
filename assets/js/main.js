ec4d6103900d0a899eca586cae6f12f02d5a5717
ruihildt <ruihildt@protonmail.com>
2019-04-30 17:09:27 +0200

    change scroll speed

--- assets/js/main.js
+++ assets/js/main.js
@@ -49,7 +49,7 @@
 
 				$nav_a
 					.scrolly({
-						speed: 1000,
+						speed: 500,
 						offset: function() { return $nav.height(); }
 					})
 					.on('click', function() {
@@ -117,7 +117,7 @@
 
 	// Scrolly.
 		$('.scrolly').scrolly({
-			speed: 1000
+			speed: 500
 		});
 
 })(jQuery);
